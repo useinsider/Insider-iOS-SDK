0a5d5c7e1c23044f380b48a5be5511601474b961
ercumentinsider <ercument.olcer@useinsider.com>
2024-06-12 16:17:27 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Insider",
+    products: [
+        .library(
+            name: "InsiderMobile",
+            targets: ["InsiderMobile"]),
+        .library(
+            name: "InsiderGeofence",
+            targets: ["InsiderGeofence"]),
+        .library(
+            name: "InsiderMobileAdvancedNotification",
+            targets: ["InsiderMobileAdvancedNotification"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "InsiderMobile",
+            url: "https://mobilesdk.useinsider.com/iOS/13.5.0/InsiderMobileIOSFramework.zip",
+            checksum: "ec1014e3b32e0212ec5480ec3db655f87bb8990c2fd6039a4226a171c700bdbc"
+        ),
+        .binaryTarget(
+            name: "InsiderGeofence",
+            url: "https://mobilesdk.useinsider.com/iOS/InsiderGeofence/1.2.0/InsiderGeofenceIOSFramework.zip",
+            checksum: "c180df9943ebc66a13717d6ddfc14d7cb225c98b3c5bfcaad5236f5f2127aba5"
+        ),
+        .binaryTarget(
+            name: "InsiderMobileAdvancedNotification",
+            url: "https://mobilesdk.useinsider.com/iOSNotification/1.3.0/InsiderMobileAdvancedNotification.zip",
+            checksum: "36f39d03f007a24fd75ea42d65f3565b2506e12bd777941a14b136143293dc01"
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,7 @@
+# Insider SDK
+
+Insider iOS SDK provides a set of frameworks for integrating Insider services into your iOS application. The SDK includes modules for mobile interaction, geofencing, and advanced notifications.
+
+## Installation
+
+See Insider's [iOS SDK Setup](https://academy.useinsider.com/docs/ios-basic-sdk-setup) for documentation.
